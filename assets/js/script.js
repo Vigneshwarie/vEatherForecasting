3ee2b10d4dddc40f8e73f248e904fb91367bca23
Vigneswari Sambandam <vigneswari@Vigneswaris-MacBook-Air.local>
2023-10-29 23:57:35 -0400

    Added function to call Weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
-var requestUrl = 'https://api.api-ninjas.com/v1/geocoding';
+var requestLocationLatLongUrl = 'https://api.api-ninjas.com/v1/geocoding';
+
+var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast";
+
+var vLatitudeLongitude_API_KEY = "VvjL07GIM+tjq1AaTRH46Q==SCecnrlco9Gox1FX";
+var vWeather_API_KEY = "c36758574d57eccdb423116f200c9c41";
+
+var latitude;
+var longitude;
 
 
 
@@ -6,19 +14,27 @@ var requestUrl = 'https://api.api-ninjas.com/v1/geocoding';
 
 
 $("#searchButton").on('click', function () {  
-     console.log("hello");
      var cityName = $("input[name=cityDataList]").val();
      if (cityName !== "") {
           console.log(cityName);
-          requestUrl = requestUrl + "?city=" + cityName + "";
+          requestLocationLatLongUrl = requestLocationLatLongUrl + "?city=" + cityName + "";
 
           $.ajax({
           method: 'GET',
-          url: requestUrl,
-          headers: { 'X-Api-Key': 'VvjL07GIM+tjq1AaTRH46Q==SCecnrlco9Gox1FX'},
+          url: requestLocationLatLongUrl,
+          headers: { 'X-Api-Key': vLatitudeLongitude_API_KEY},
           contentType: 'application/json',
           success: function(result) {
                console.log(result);
+               if (result.length > 1) {
+                    latitude = result[0].latitude;
+                    longitude = result[0].longitude;
+               }
+               else {
+                    latitude = result.latitude;
+                    longitude = result.longitude;
+               }
+               getWeatherDetails(latitude, longitude);
           },
           error: function ajaxError(jqXHR) {
                console.error('Error: ', jqXHR.responseText);
@@ -28,8 +44,20 @@ $("#searchButton").on('click', function () {
      else {
           window.alert("Please select the city!");
      }
-
-     
-
-    
 });
+
+
+function getWeatherDetails(pLatitude, pLongitude) {
+     weatherUrl = weatherUrl + "?lat=" + pLatitude + "&lon=" + pLongitude + "&appid={" + vWeather_API_KEY + "}";
+
+     console.log(weatherUrl);
+
+     $.ajax({
+     url: weatherUrl,
+     method: 'GET',
+     }).then(function (response) {
+     console.log('AJAX Response \n-------------');
+     console.log(response);
+     });
+
+}
